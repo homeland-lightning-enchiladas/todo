35d4d2afaeda340bf11faa427f7a8215e41b267f
Austin Evans <evansau1@msu.edu>
2019-04-04 04:59:10 +0000

    Init commit

---
+++ todo/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for todo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/todo')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/todo/egg_cache")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
+
+
+application = get_wsgi_application()
